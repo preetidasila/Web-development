6c384ff8f3dec968ba0381c4ef81f4c2e49af441
preetidasila <68819503+preetidasila@users.noreply.github.com>
2021-02-08 10:41:09 +0530

    Update abc.php

--- abc.php
+++ abc.php
@@ -5,12 +5,12 @@ include('config.php');
 if(isset($_POST['submit'])){
     $username = $_POST['username'];
     $email = $_POST['email'];
-    $gender = $_POST['gender'];
+    $contact = $_POST['contact'];
     $city = $_POST['city'];
     $branch = $_POST['branch'];
     $year = $_POST['year'];
 
-    $sql = "INSERT INTO `users` (`Username`, `Email`, `Gender`, `City`) VALUES ('$username', '$email', '$gender', '$city')";
+    $sql = "INSERT INTO `users` (`Username`, `Email`, `Contact`, `City`) VALUES ('$username', '$email', '$contact', '$city')";
     $sql1="INSERT INTO `student details`(`Username`,`Branch`,`Year`)VALUES('$username','$branch','$year')";
     mysqli_query($conn, $sql);
     mysqli_query($conn,$sql1);
@@ -28,10 +28,7 @@ else{
 	<form method="POST" action="abc.php">
 	USERNAME<input type="text" name="username"  required><br>
 	EMAIL<input type="email" name="email" required><br>
-	GENDER<input type="radio" id="Male" name="gender" value="Male">
-	<label for="Male">Male</label>
-	<input type="radio"  id-="Female" name="gender" value="Female" checked>
-	<label for="Female">Female</label><br>
+	CONTACT<input type="text" name="contact" required><br>
 	SELECT CITY<select name="city">
 		<option value="Dehradun">Dehradun</option>
 		<option value="Delhi">Delhi</option>
