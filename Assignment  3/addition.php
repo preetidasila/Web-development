22e0b7c9a99fb739f367aa3dbfa2768e5e7a3d4e
preetidasila <68819503+preetidasila@users.noreply.github.com>
2021-02-11 20:15:20 +0530

    Update addition.php

--- Assignment  3/addition.php
+++ Assignment  3/addition.php
@@ -1,1 +1,31 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+	<title>addition</title>
+</head>
+<body>
+	<form method="POST" action="addition.php">
+	Variable1<input type="text" name="v1" ><br>
+	Variable2<input type="text" name="v2" ><br>
+	<input type="submit" name="submit" value="Submit">
+</form>
+</body>
+</html>
+<?php
+	if(isset($_POST['submit']))
+	{
+		if(empty($_POST['v1'])|| empty($_POST['v2']))
+		{
+			echo "Enter values first";
+		}
+		else
+		{
+			$sum=$_POST['v1']+$_POST['v2'];
+			echo" SUM=$sum";
+		}
+	}
+	else
+	{
+		echo"NO input ";
+	}
+?>
