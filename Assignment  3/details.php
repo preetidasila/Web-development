c9cedd3be007b25edf8e887fa73772922586f830
preetidasila <68819503+preetidasila@users.noreply.github.com>
2021-02-15 13:38:43 +0530

    Update details.php

--- Assignment  3/details.php
+++ Assignment  3/details.php
@@ -7,9 +7,15 @@ $result=mysqli_query($conn,$sql);
 if($result->num_rows >0)
 {
 	?>
+	<html>
+<head>
+	<title>Data table</title>
+</head>
+<body>
 	<table border="1px">
 	<thead>
 	<tr>
+		<th>Id</th>
 		<th>Username</th>
 		<th>Email</th>
 		<th>Gender</th>
@@ -24,12 +30,13 @@ if($result->num_rows >0)
 	{
 		?>
 		<tr>
+			<td><?php echo $row['id']?></td>
 		<td><?php echo $row['Username']?></td>
-		<td><?php echo $row['Email']?></td>
+		<td><?php echo $row['Email']?></td>`
 		<td><?php echo $row['Gender']?></td>
 		<td><?php echo $row['City']?></td>
 		<td><a href="edit.php?id=<?php echo $row['id']?>"><input type="button" value="Edit"></a></td>
-		<td><input type="button" value="Delete"></td>
+		<td><a href="delete.php?id=<?php echo $row['id']?>"><input type="button" onclick="return remove()" value = "Delete"></a></td>
 	</tr>
 
 	<?php }
@@ -39,12 +46,19 @@ if($result->num_rows >0)
 <?php
 }
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Data table</title>
-</head>
-<body>
+<script type="text/javascript">
+		function remove()
+		{
+			if(confirm("Do you want to delete this record permanently?"))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	</script>
 
 </body>
 </html>
